9ff718f0b2bd22abcdf6ad07cea0ecfed9e09754
Hayes Choy <hayeselnut2127@gmail.com>
2020-08-27 01:28:47 +1000

    refactored code

--- webscraper.py
+++ webscraper.py
@@ -1,18 +1,32 @@
 from bs4 import BeautifulSoup
 import requests
 
-response = requests.get("https://www.handbook.unsw.edu.au")
+HANDBOOK_URL = "https://www.handbook.unsw.edu.au"
 
-if not response.ok:
-    print(f"ERROR get summoner_id: status code {response.status_code}")
-    exit(1)
+def get_faculties():
+    response = requests.get(HANDBOOK_URL)
 
-home_page = BeautifulSoup(response.content, "html.parser")
+    if not response.ok:
+        print(f"ERROR get summoner_id: status code {response.status_code}")
+        exit(1)
 
-faculties = home_page.find(id="tab_OrgUnits").find_all("a", class_="a-browse-tile")
+    home_page = BeautifulSoup(response.content, "html.parser")
 
-for f in faculties:
-    link = f["href"]
-    faculty = f.find("h3", class_="h4").text
+    faculties_elem = home_page.find(id="tab_OrgUnits").find_all("a", class_="a-browse-tile")
 
-    print(link, faculty)
+    faculties = []
+    for f in faculties_elem:
+        link = f["href"]
+        faculty = f.find("h3", class_="h4").text
+
+        print(link, faculty)
+        faculties.append({
+            "faculty": faculty,
+            "link": link
+        })
+
+    return faculties
+
+HANDBOOK_FACULTIES = get_faculties()
+
+print(HANDBOOK_FACULTIES)
