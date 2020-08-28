2da0bb28313e2d0517c4acd9eaf6265df6b5b2dc
Hayes Choy <hayeselnut2127@gmail.com>
2020-08-29 01:58:07 +1000

    put degrees of engineering_cache into engineering_degrees

--- webscraper/temp.py
+++ webscraper/temp.py
@@ -1,13 +1,31 @@
 import json
 
-with open("list_of_courses.json", "r") as read_file:
-    courses = json.load(read_file)
 
-for code in courses:
-    link = courses[code]["link"]
+ENGINEERING_DEGREES = {}
+with open("engineering_cache.json", "r") as read_file:
+    a = json.load(read_file)
+    degrees = a["degrees"]
 
-    if code == "null":
-        continue
+    for code in degrees:
+        degree_name = degrees[code]["degree_name"]
+        degree_code = degrees[code]["degree_code"]
+        degree_units = degrees[code]["degree_units"]
 
-    if "courses" not in link:
-        print(f"{code}: {link}")
+        core_courses = {}
+        for segment in degrees[code]["core_courses"]:
+            core_courses[segment] = []
+            for course in  degrees[code]["core_courses"][segment]:
+                if course["code"] == None:
+                    core_courses[segment].append(course["name"])
+                    continue
+                core_courses[segment].append(course["code"])
+
+        ENGINEERING_DEGREES[code] = {
+            "degree_name": degree_name,
+            "degree_code": degree_code,
+            "degree_units": degree_units,
+            "core_courses": core_courses
+        }
+
+with open("engineering_degrees.json", "w") as write_file:
+        json.dump(ENGINEERING_DEGREES, write_file)
