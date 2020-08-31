e0f3c690bd1112a0089633e35fd46e8e5c240645
Hayes Choy <hayeselnut2127@gmail.com>
2020-08-31 22:36:47 +1000

    cleaned up files

--- scraper1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# echo "First argument is $1"
-useful=`curl --silent "https://www.handbook.unsw.edu.au/undergraduate/specialisations/2020/SENGAH" | grep -A 4 'data-bucket="1"' | sed 's/--/&\n/g'`
-
-echo $useful
