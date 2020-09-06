d6a68d3391d53cc0b3ff9883021fc11753515bf6
Hayes Choy <hayes.choy@student.unsw.edu.au>
2020-09-06 23:13:37 +1000

    scraped specialisations/majors/minors, and theprogram can calculate missing units as well!

--- src/webscraper/get_program_info.py
+++ src/webscraper/get_program_info.py
@@ -12,7 +12,7 @@ PROGRAMS = {}
 
 WAIT = 20
 REGEX_COURSE_CODE = "[A-Z]{4}\d{4}"
-REGEX_SPECIALISATION_CODE = "[A-Z]{5}[H\d]"
+REGEX_SPECIALISATION_CODE = "^[A-Z]{5}[H\d]$"
 REGEX_PROGRAM_CODE = "\d{4}"
 
 @scrape.return_null_on_failure
@@ -43,7 +43,7 @@ def get_program_duration(html):
     return int(html.find("h4", text=re.compile("Typical duration")).find_next_sibling("div").text.split(" ")[0])
 
 @scrape.return_null_on_failure
-def get_level_degrees(level_html):
+def get_sublevels(level_html):
     pass
 
 @scrape.return_null_on_failure
@@ -55,30 +55,51 @@ def get_level_requirements(level_html):
     return int(level_html.find("small").text.split(" ")[0])
 
 @scrape.return_null_on_failure
-def get_program_structure(html):
+def get_program_structure(html, total_units):
     degree_structure = {}
     program_structure = html.find(id="ProgramStructure").find_all(class_="css-8x1vkg-Box-Card-EmptyCard-css-SAccordionContainer e1450wuy4")
 
+    unknowns = 0
+    units_used = 0
+    unknown_key = None
     for level in program_structure:
+        level_requirement = get_level_requirements(level)
         level_name = get_level_name(level)
 
-        degree_structure[level_name] = {
-            "name": level_name,
-            "units_required": get_level_requirements(level)
-            # "courses": get_level_courses(level)
-        }
+        degree_structure[level_name] = level_requirement
+
+        if level_requirement == None:
+            unknown_key = level_name
+            unknowns += 1
+        else:
+            units_used += level_requirement
+
+    # CALCULATE UNITS IF POSSIBLE
+    if unknowns == 1 and units_used <= total_units:
+        degree_structure[unknown_key] = total_units - units_used
 
     return degree_structure
 
+@scrape.return_null_on_failure
+def get_subprograms(html):
+    return {
+        "majors": html.find(id="ProgramStructure").find_all(text=re.compile("^[A-Z]{5}1$")),
+        "minors": html.find(id="ProgramStructure").find_all(text=re.compile("^[A-Z]{5}2$")),
+        "honours": html.find(id="ProgramStructure").find_all(text=re.compile("^[A-Z]{5}H"))
+    }
+
 def get_program_info(html):
+    units = get_program_units(html)
+
     return {
         "name": get_program_name(html),
         "code": get_program_code(html),
         "desc": get_program_desc(html),
         "faculty": get_program_faculty(html),
-        "units": get_program_units(html),
+        "units": units,
         "duration": get_program_duration(html),
-        "structure": get_program_structure(html)
+        "structure": get_program_structure(html, units),
+        "degrees_involved": get_subprograms(html)
     }
 
 def get_programs(browser, faculty_links):
