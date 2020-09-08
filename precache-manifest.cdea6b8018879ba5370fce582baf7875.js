849801dd7b8a5cd84eb87556259245ebccca3aba
Hayes Choy <hayeselnut2127@gmail.com>
2020-09-08 21:11:56 +1000

    Updates

---
+++ precache-manifest.cdea6b8018879ba5370fce582baf7875.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "085888a845b1fff3a7eb33a585144383",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80698a982843f265e289",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "5174a5bf8037d9da3aeb",
+    "url": "/static/js/2.dd256d0a.chunk.js"
+  },
+  {
+    "revision": "db30d36cc67fe41fff60f7a6d4e2e2fd",
+    "url": "/static/js/2.dd256d0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80698a982843f265e289",
+    "url": "/static/js/main.50cf2f2e.chunk.js"
+  },
+  {
+    "revision": "c131d55fa3b832ac7601",
+    "url": "/static/js/runtime-main.58383973.js"
+  }
+]);
