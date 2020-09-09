c716ada325f63af7d26eff44c271d7c3e4f1e30a
James Ji <jamesx.ji@gmail.com>
2020-09-09 14:58:49 +1000

    .

--- src/App.js
+++ src/App.js
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-=======
 import React, {Component} from 'react';
 import {
   BrowserRouter as Router,
@@ -44,7 +19,6 @@ class App extends Component {
       </Router>
     )
   }
->>>>>>> a00b60dded967517e3f87e7ff66373631a5707f4
 }
 
 export default App;
