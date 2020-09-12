d73b9d339e01764b3bf4897529d7f301ade31c61
VincentXiao-jpg <vincent.xiao3@gmail.com>
2020-09-13 02:04:38 +1000

    Testing

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import {
   Route,
   Switch,
   Link,
-  Redirect
+  Redirect,
+  HashRouter
 } from 'react-router-dom';
 
 import BESengah from './maps/EngineeringHonoursSoftware/map.js';
@@ -13,10 +14,18 @@ import Teaching from './components/teaching.js';
 class App extends Component {
   render() {
     return (
-      <Router>
-        <Route path="/SoftwareEngineering" component={BESengah} />
-        <Route path="/Teaching" component={Teaching}/>
-      </Router>
+      <HashRouter basename='/'>
+        <div>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/SoftwareEngineering">Software Engineering</Link></li>
+          </ul>
+
+          <hr />
+          <Route path="/SoftwareEngineering" component={BESengah} />
+          <Route path="/Teaching" component={Teaching}/>
+        </div>
+      </HashRouter>
     )
   }
 }
