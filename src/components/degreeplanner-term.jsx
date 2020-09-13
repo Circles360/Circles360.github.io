2cfc78835b0b07bf21ff23f43f6c29336712860d
Hayes Choy <hayeselnut2127@gmail.com>
2020-09-14 00:07:53 +1000

    terms segments show if invalid course is placed inside it

--- src/components/degreeplanner-term.jsx
+++ src/components/degreeplanner-term.jsx
@@ -5,11 +5,25 @@ import Course from "./degreeplanner-course"
 
 import { Droppable } from "react-beautiful-dnd"
 
+const SUCCESS = "#e5fbe5";
+const ERROR = "#ffebeb";
+
 const checkTermAvailability = (props, courseId) => {
-    const termId = props.term.id;
+    const termId = props.term.id.substring(1, 3);
     const termAvailability = props.allCourses[courseId].termsAvailable;
 
-    return termAvailability.includes(termId) ? "#e5fbe5" : "#ffebeb";
+    return termAvailability.includes(termId) ? SUCCESS : ERROR;
+}
+
+const checkValidCourses = (props) => {
+    const termId = props.term.id.substring(1, 3);
+
+    for (const courseId of props.term.courseIds) {
+        if (!props.allCourses[courseId].termsAvailable.includes(termId)) {
+            return ERROR
+        }
+    }
+    return "white"
 }
 
 export default class Term extends React.Component {
@@ -18,7 +32,7 @@ export default class Term extends React.Component {
             <Container>
                 <Droppable droppableId={this.props.term.id}>
                     {(provided, snapshot) => (
-                        <Segment style={{backgroundColor: snapshot.isDraggingOver ? checkTermAvailability(this.props, snapshot.draggingOverWith) : "white", transition: "0.2s ease"}}>
+                        <Segment style={{backgroundColor: snapshot.isDraggingOver ? checkTermAvailability(this.props, snapshot.draggingOverWith) : checkValidCourses(this.props), transition: "0.2s ease"}}>
                             <Header as="h3">{this.props.term.title}</Header>
                             <div ref={provided.innerRef} {...provided.droppableProps} style={{minHeight: "150px"}}>
                                 {this.props.courses.map((course, index) => <Course key={course.id} course={course} index={index} />)}
