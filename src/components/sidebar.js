3ff5ef5ba61c5689aab0b68a9bface2bc039858f
Hayes Choy <hayeselnut2127@gmail.com>
2020-09-14 21:06:13 +1000

    got all relevant courses

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,98 +1,82 @@
 import React, {useState} from 'react';
-import DropdownDegrees from "./dropdownDegrees.js"
-import SidebarModal from "./sidebar-modal.js"
-import { Icon, Button, Container, Modal, Header, Dropdown, Grid, Message } from 'semantic-ui-react'
-import ScrollTo from "react-scroll-into-view"
+import SidebarModal from "./sidebar-modal.js";
+import { Icon, Button, Container, Header, Dropdown, Grid, Segment, Message, Label } from 'semantic-ui-react';
+import ScrollTo from "react-scroll-into-view";
+import specialisationsJSON from "../webscraper/specialisations.json";
+import coursesJSON from "../webscraper/courses.json";
 
-const example = [
-    { key: 'af', value: 'af', flag: 'af', text: 'Afghanistan' },
-    { key: 'ax', value: 'ax', flag: 'ax', text: 'Aland Islands' },
-    { key: 'al', value: 'al', flag: 'al', text: 'Albania' },
-    { key: 'dz', value: 'dz', flag: 'dz', text: 'Algeria' },
-    { key: 'as', value: 'as', flag: 'as', text: 'American Samoa' },
-    { key: 'ad', value: 'ad', flag: 'ad', text: 'Andorra' },
-    { key: 'ao', value: 'ao', flag: 'ao', text: 'Angola' },
-    { key: 'ai', value: 'ai', flag: 'ai', text: 'Anguilla' },
-    { key: 'ag', value: 'ag', flag: 'ag', text: 'Antigua' },
-    { key: 'ar', value: 'ar', flag: 'ar', text: 'Argentina' },
-    { key: 'am', value: 'am', flag: 'am', text: 'Armenia' },
-    { key: 'aw', value: 'aw', flag: 'aw', text: 'Aruba' },
-    { key: 'au', value: 'au', flag: 'au', text: 'Australia' },
-    { key: 'at', value: 'at', flag: 'at', text: 'Austria' },
-    { key: 'az', value: 'az', flag: 'az', text: 'Azerbaijan' },
-    { key: 'bs', value: 'bs', flag: 'bs', text: 'Bahamas' },
-    { key: 'bh', value: 'bh', flag: 'bh', text: 'Bahrain' },
-    { key: 'bd', value: 'bd', flag: 'bd', text: 'Bangladesh' },
-    { key: 'bb', value: 'bb', flag: 'bb', text: 'Barbados' },
-    { key: 'by', value: 'by', flag: 'by', text: 'Belarus' },
-    { key: 'be', value: 'be', flag: 'be', text: 'Belgium' },
-    { key: 'bz', value: 'bz', flag: 'bz', text: 'Belize' },
-    { key: 'bj', value: 'bj', flag: 'bj', text: 'Benin' }
-]
+const REGEX_COURSE_CODE = /[A-Z]{4}\d{4}/g;
 
-const Sidebar = (props) => {
-    return (
-        <Container style={{paddingLeft: "10px", paddingRight: "10px"}}>
-            <Header as="h1" textAlign="center" style={{marginTop: "10px"}}>Circles</Header>
-            <Message info>
-                <p>Circles is a visual degree planner for UNSW students. Choose your program and degree below to begin!</p>
-                <SidebarModal />
-            </Message>
+const getSelectedCourses = (specialisationCode) => {
+    const levels = Object.keys(specialisationsJSON[specialisationCode].structure);
 
-            <Grid stretched>
-                <Grid.Row>
-                    <Container>
-                        <Header as="h3" textAlign="center">Choose your courses</Header>
-                        <Header as="h4" style={{marginBottom: 0}}>Level 1 Core Courses</Header>
-                        <Dropdown
-                            fluid
-                            selection
-                            search
-                            multiple
-                            options={example}
-                            placeholder="Select courses"
-                        />
+    const rawCourses = levels.filter(levelName => {
+        const coursesInLevel = specialisationsJSON[specialisationCode].structure[levelName].courses;
+        if (coursesInLevel == null) return false;
+        if (coursesInLevel.length === 1 && coursesInLevel.includes("ANY COURSE")) return false;
+        return true;
+    }).map(levelName => specialisationsJSON[specialisationCode].structure[levelName].courses.flat().filter(c => c != "ANY LEVEL")).flat();
 
-                        <Header as="h4" style={{marginBottom: 0}}>Level 2 Core Courses</Header>
-                        <Dropdown
-                            fluid
-                            selection
-                            search
-                            multiple
-                            options={example}
-                            placeholder="Select courses"
-                        />
+    console.log(rawCourses);
+    const allCourseIds = Object.keys(coursesJSON);
 
-                        <Header as="h4" style={{marginBottom: 0}}>Level 3 Core Courses</Header>
-                        <Dropdown
-                            fluid
-                            selection
-                            search
-                            multiple
-                            options={example}
-                            placeholder="Select courses"
-                        />
+    const coursesInThisSpecialisation = [];
+    rawCourses.forEach(courseId => {
+        if (courseId.match(REGEX_COURSE_CODE)) {
+            coursesInThisSpecialisation.push(courseId);
+        } else if (courseId.match(/^[A-Z]{4}\d$/)) {
+            const relevantCourses = allCourseIds.filter(c => c.includes(courseId));
+            console.log("relevant", relevantCourses);
+            relevantCourses.forEach(c => {
+                if (coursesInThisSpecialisation.includes(c)) return;
+                coursesInThisSpecialisation.push(c);
+            })
+        }
+    });
+
+    return coursesInThisSpecialisation.map(c => (<Label style={{margin: "2px"}}>{c}</Label>));
+}
+class Sidebar extends React.Component {
+    state = {
+
+    };
+
+    render() {
+
+        return (
+            <Container style={{paddingLeft: "10px", paddingRight: "10px"}}>
+                <Header as="h1" textAlign="center" style={{marginTop: "10px"}}>Circles</Header>
+                <Message info>
+                    <p>Circles is a visual degree planner for UNSW students. Choose your program and degree below to begin!</p>
+                    <SidebarModal />
+                </Message>
+                <Grid stretched>
+                    <Grid.Row>
+                        <Container>
+                            <Header as="h3" textAlign="center">Choose your courses</Header>
+                            {getSelectedCourses("SENGAH")}
+                        </Container>
+                    </Grid.Row>
+
+                    <Container textAlign="center">
+                        <ScrollTo selector="#DegreePlanner">
+                            <Button
+                                animated="vertical"
+                                color="red"
+                            >
+                                <Button.Content visible>Generate degree planner</Button.Content>
+                                <Button.Content hidden>
+                                    <Icon name="arrow down" />
+                                </Button.Content>
+                            </Button>
+                        </ScrollTo>
+
                     </Container>
-                </Grid.Row>
+                </Grid>
+            </Container>
 
-                <Container textAlign="center">
-                    <ScrollTo selector="#DegreePlanner">
-                        <Button
-                            animated="vertical"
-                            color="red"
-                        >
-                            <Button.Content visible>Generate degree planner</Button.Content>
-                            <Button.Content hidden>
-                                <Icon name="arrow down" />
-                            </Button.Content>
-                        </Button>
-                    </ScrollTo>
-
-                </Container>
-            </Grid>
-        </Container>
-
-    );
+        );
+    }
 }
 
 
