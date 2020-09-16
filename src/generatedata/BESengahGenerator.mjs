2a74f406d3c46033dad967212b7f3ee42baa77e9
James Ji <jamesx.ji@gmail.com>
2020-09-16 12:29:30 +1000

    fixed bug related to generator not sorting terms properly

--- src/generatedata/BESengahGenerator.mjs
+++ src/generatedata/BESengahGenerator.mjs
@@ -168,9 +168,6 @@ courses_output = courses_output.filter(function(course) {
 
 // Go through the unlocks for each course and if it is not a node in our graph,
 // delete it. If the array is empty, set it to null
-// NEW: SORT THE TERMS IN ORDER for nicer display when hovering
-// NEW: Cut off prerequisites at the front in "raw" so it looks nicer when displaying
-
 for (var course of courses_output) {
     if (course.data.unlocks === null) continue;
     course.data.unlocks = course.data.unlocks.filter((unlockCourse) => {
@@ -180,13 +177,20 @@ for (var course of courses_output) {
     if (course.data.unlocks.length === 0) {
         course.data.unlocks = null;
     }
-    
-    if (course.data.terms !== null) course.data.terms.sort();
+}
+
+// NEW: SORT THE TERMS IN ORDER for nicer display when hovering
+// NEW: Cut off prerequisites at the front in "raw" so it looks nicer when displaying
+for (var course of courses_output) {
+    if (course.data.terms !== null) {
+        console.log(course.id + " SORTING TERMS " + course.data.terms);
+        course.data.terms.sort();
+        console.log(course.data.terms);
+    }
     if (course.data.conditions.raw !== null) {
         course.data.conditions.raw = course.data.conditions.raw.replace(/Pre-?requisite: /, "");
     }
 }
-
 
 // Add course header
 courses_output.unshift({
