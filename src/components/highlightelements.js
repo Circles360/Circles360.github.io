4034612287f9c4ef1d884b3440d1e07378396def
James Ji <jamesx.ji@gmail.com>
2020-09-17 10:48:42 +1000

    accidentally reverted highlight elements as well

--- src/components/highlightelements.js
+++ src/components/highlightelements.js
@@ -16,14 +16,14 @@ const selectedEdgeStatic = {
 }
 
 const hoverEdge = {
-    stroke: "#454545",
+    stroke: "#e94646",
     strokeWidth: 2,
     opacity: 1,
     transition: "0.3s ease"
 }
 
 const potentialHoverEdge = {
-    stroke: "#454545",
+    stroke: "#e94646",
     strokeWidth: 2,
     opacity: 1,
     transition: "0.3s ease"
@@ -62,48 +62,14 @@ export default function highlightElements(elements, selectedNodes, selectedEdges
         // It is a node
         if (selectedNodes.hasOwnProperty(e.id)) {
             // SELECTED NODE
-            const selected = e.selected;
-            return {...e, style: selected};
-
-            // if (e.style.background === 'white') {
-            //     // It was previously a selectable node. Adjust colours accordingly
-            //     const backgroundColour = e.style.color;
-            //     // return {...e, style: {...e.style, color: 'white', background: backgroundColour, filter: 'brightness(1.15)'}}
-            //     return {...e};
-            // } else {
-            //     // It was an unselected node or an already selected node. Simply make sure its brightness is adjusted
-            //     // return {...e, style: {...e.style, filter: 'brightness(1.15)'}};
-            //     return {...e};
-            // }
+            return {...e, style: {...e.style, color: e.textSelectedColour, background: e.selectedColour}};
         } else if (selectableNodes.hasOwnProperty(e.id)) {
             // NOT SELECTED BUT CAN BE SELECTED
-            const canSelect = e.canSelect;
-            return {...e, style: canSelect};
-            // 
-            // if (e.style.background === 'white') {
-            //     // It was already a selectable node. Do not need to adjust
-            //     return {...e};
-            // } else {
-            //     // It was an unselected node or an already selected node. Adjust text colour and background
-            //     const textColour = e.style.background;
-            //     // return {...e, style: {...e.style, color: textColour, background: 'white', filter: 'brightness(1.15)'}};
-            //     return {...e};
-            // }
+            console.log(e.textColour);
+            return {...e, style: {...e.style, color: e.textColour, background: e.selectableColour}};
         } else {
-            // NOT SELECTED AND SHOULD NOT BE SHOWN AS CAN SELECTED
-            const cannotSelect = e.cannotSelect;
-            return {...e, style: cannotSelect};
-            // CONDITIONS NOT MET YET
-            // if (e.style.background === 'white') {
-            //     // It was a selectable node previously. Adjust colours accordingly
-            //     const backgroundColour = e.style.color;
-            //     return {...e};
-            //     // return {...e, style: {...e.style, color: 'white', background: backgroundColour, filter: 'brightness(0.65)'}}; 
-            // } else {
-            //     // It was either unselected or an already selected node. Simply adjust brightness
-            //     // return {...e, style: {...e.style, filter: 'brightness(0.65)'}};
-            //     return {...e};
-            // }
+            // UNSELECTED NODE
+            return {...e, style: {...e.style, color: e.textColour, background: 'white'}};
         }
     })
 
