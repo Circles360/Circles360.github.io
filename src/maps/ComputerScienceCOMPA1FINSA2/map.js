ba0ca63c2bfe50ddefd550c08504383e56d164d0
VincentXiao-jpg <vincent.xiao3@gmail.com>
2020-09-18 18:38:41 +1000

    Fixed some stuff

--- src/maps/ComputerScienceCOMPA1FINSA2/map.js
+++ src/maps/ComputerScienceCOMPA1FINSA2/map.js
@@ -271,7 +271,7 @@ const ComputerScienceCOMPA1FINSA2 = () => {
                                     onElementClick={onElementClick}
                                     minZoom={0.38}
                                     //setInitTransform={TransformUpdater({x: 100, y: 100, z: 1})}
-                                    nodesDraggable={true}
+                                    nodesDraggable={false}
                                     onNodeMouseEnter={onNodeMouseEnter}
                                     onNodeMouseLeave={onNodeMouseLeave}
                                     selectNodesOnDrag={false}
