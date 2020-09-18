8768f6d35c5fb64cce5ed497e41336bdd905e000
VincentXiao-jpg <vincent.xiao3@gmail.com>
2020-09-18 18:39:26 +1000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.54aa97745b8e22210fd5bde42e0d5128.js"
+  "/precache-manifest.7194915eca545c40b75c6dea7b626efa.js"
 );
 
 self.addEventListener('message', (event) => {
