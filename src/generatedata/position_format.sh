6121b7cf7040546ebf2ac1dc6a8de76ff80ee2a4
James Ji <jamesx.ji@gmail.com>
2021-01-27 17:22:28 +1100

    improved position_format so it is almost fully automated

--- src/generatedata/position_format.sh
+++ src/generatedata/position_format.sh
@@ -5,10 +5,10 @@ output=""
 # Formats the console output positions
 while read -r line
 do
-    new_line=`echo $line | sed 's/[^{]*{/{/' | sed 's/,$//'`
-    output="${output},\n${new_line}"
-done < "../maps/$1/position.json"
+    new_line=`echo $line | sed 's/[^{]*//' | sed 's/,$//'`
+    output="${output}\n${new_line},"
+done < "../flowchart/maps/$1/position.json"
 
-echo "[\n" > "../maps/$1/position.json"
-echo $output >> "../maps/$1/position.json"
-echo "\n]" >> "../maps/$1/position.json"
+echo "[\n" > "../flowchart/maps/$1/position.json"
+echo "$output" | sed '$ s/,$//' >> ../flowchart/maps/$1/position.json
+echo "\n]" >> "../flowchart/maps/$1/position.json"
