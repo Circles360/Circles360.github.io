0e793a49bc83de1869a03c78d20a8dd400d5610d
James Ji <jamesx.ji@gmail.com>
2024-03-03 16:53:32 +1100

    update: csesoc -> devsoc

--- src/maps/Homepage/map.js
+++ src/maps/Homepage/map.js
@@ -39,7 +39,7 @@ const Homepage = () => {
                             The visual degree planner for UNSW undergraduate students
                             <div>
                                 <Message warning>
-                                    Please visit the new Circles being worked on by the CSESoc Projects team at <a href="https://circles.devsoc.app" target="_blank" rel="noopener noreferrer">https://circles.devsoc.app</a>
+                                    Please visit the new Circles being worked on by the DevSoc team at <a href="https://circles.devsoc.app" target="_blank" rel="noopener noreferrer">https://circles.devsoc.app</a>
                                 </Message>
                             </div>
                         </Header.Subheader>
